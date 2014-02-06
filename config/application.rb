6d876092f29ba34805bdc44e64c93d88a7692787
Vivek Koranampatty Surendran <vivek@anumira.com>
2014-02-06 16:32:30 +0530

    push

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,6 @@ module Apptracksys
     # -- all .rb files in that directory are automatically loaded.
     config.generators.stylesheets = false
     config.generators.javascripts = false
-    config.assets.initialize_on_precompile=false
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
